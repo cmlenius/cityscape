951b417301e9103ef161713c02bfe08210602ed3
Cameron Lenius <cameron.lenius@kirasystems.com>
2020-02-29 16:09:15 -0500

    add starfield background

--- js/cityscape.js
+++ js/cityscape.js
@@ -1,16 +1,14 @@
 function generateScene() {
-    /*
-    // Sky
-    var skyGeometry = new THREE.SphereGeometry(50000, 25, 25);
-    var material = new THREE.MeshPhongMaterial({
-        map: new THREE.TextureLoader().load( '../textures/sky.png' ),
-    });
-    var sky = new THREE.Mesh(skyGeometry, material);
-    sky.material.side = THREE.BackSide;
-    scene.add(sky);
-    */
 
-    // Floor
+    // Sky
+    var skyBox = new THREE.BoxGeometry(2500, 2500, 2500);
+    var skyBoxMaterial = new THREE.MeshBasicMaterial({
+        map: genStarField(600, 2048, 2048),
+    	side: THREE.BackSide
+    });
+    var sky = new THREE.Mesh(skyBox, skyBoxMaterial);
+    scene.add(sky);
+   
    /*
     let planeTexture = new THREE.Texture(genPlaneTexture());
     planeTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
@@ -19,6 +17,7 @@ function generateScene() {
     planeTexture.wrapT = THREE.RepeatWrapping;
     */
     
+    // Floor
     let planeGeometry = new THREE.PlaneGeometry(GRID_SIZE, GRID_SIZE, 32);
     let planeMaterial = new THREE.MeshBasicMaterial({
         //map: planeTexture,
@@ -29,7 +28,6 @@ function generateScene() {
     let plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotation.x = Math.PI / 2;
     scene.add(plane);
-    
 
     // Ambient Light
     let hemLight = new THREE.HemisphereLight(0xfffff0, 0x101020, 2);
@@ -37,7 +35,31 @@ function generateScene() {
     scene.add(hemLight);
 
     genBuildings();
-    //addLight(0.995, 0.5, 0.9, -1024, 200, -1024);
+}
+
+function genStarField(numOfStars, width, height) {
+    let canvas = document.createElement('canvas');
+    canvas.width = width;
+	canvas.height = height;
+
+	let ctx = canvas.getContext('2d');
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, width, height);
+
+	for (let i = 0; i < numOfStars; ++i) {
+		let radius = Math.random() * 2;
+		let x = Math.floor(Math.random() * width);
+		let y = Math.floor(Math.random() * height);
+
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'white';
+		ctx.fill();
+	}
+
+	var texture = new THREE.Texture(canvas);
+	texture.needsUpdate = true;
+	return texture;
 }
 
 function genBuildings() {
