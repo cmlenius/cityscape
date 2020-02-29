05c98f8b1e35f17d0cbfe34d157d8d820a09b1a6
Cameron Lenius <cameron.lenius@kirasystems.com>
2020-02-29 15:16:08 -0500

    remove incomplete features

--- js/cityscape.js
+++ js/cityscape.js
@@ -1,5 +1,6 @@
 function generateScene() {
-
+    /*
+    // Sky
     var skyGeometry = new THREE.SphereGeometry(50000, 25, 25);
     var material = new THREE.MeshPhongMaterial({
         map: new THREE.TextureLoader().load( '../textures/sky.png' ),
@@ -7,23 +8,28 @@ function generateScene() {
     var sky = new THREE.Mesh(skyGeometry, material);
     sky.material.side = THREE.BackSide;
     scene.add(sky);
+    */
 
     // Floor
+   /*
     let planeTexture = new THREE.Texture(genPlaneTexture());
     planeTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
     planeTexture.needsUpdate = true;
     planeTexture.wrapS = THREE.RepeatWrapping;
     planeTexture.wrapT = THREE.RepeatWrapping;
-
+    */
+    
     let planeGeometry = new THREE.PlaneGeometry(GRID_SIZE, GRID_SIZE, 32);
     let planeMaterial = new THREE.MeshBasicMaterial({
-        map: planeTexture,
+        //map: planeTexture,
+        color: 0x081508,
         side: THREE.DoubleSide
     });
 
     let plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotation.x = Math.PI / 2;
     scene.add(plane);
+    
 
     // Ambient Light
     let hemLight = new THREE.HemisphereLight(0xfffff0, 0x101020, 2);
@@ -36,7 +42,8 @@ function generateScene() {
 
 function genBuildings() {
     let STREET_LENGTH = 8,
-        NUM_OF_MESHES = 8;
+        NUM_OF_MESHES = 8,
+        STREET_BASE_WIDTH = 0;
 
     let ids = new Array(NUM_OF_MESHES);
     for(let i=0; i<SECTIONS; i++){
@@ -69,13 +76,13 @@ function genBuildings() {
 
     for(let i=0; i<SECTIONS; i++){
         for(let j=0; j<SECTIONS; j++){
-            let tmpx = Math.random() * 30 + 50,
-                tmpy = Math.random() * 100 + 150,
-                tmpz = Math.random() * 30 + 50;
+            let tmpx = Math.random() * 20 + 60,
+                tmpy = Math.random() * 150 + 130,
+                tmpz = Math.random() * 20 + 60;
 
             let idx = ids[i][j];
-            buildingMeshes[idx].position.x = SECTION_SIZE*i + SECTION_SIZE/2 - GRID_SIZE/2;
-            buildingMeshes[idx].position.z = SECTION_SIZE*j + SECTION_SIZE/2 - GRID_SIZE/2;
+            buildingMeshes[idx].position.x = SECTION_SIZE*i + SECTION_SIZE/2 - GRID_SIZE/2 + Math.random()*60;
+            buildingMeshes[idx].position.z = SECTION_SIZE*j + SECTION_SIZE/2 - GRID_SIZE/2 + Math.random()*60;
             buildingMeshes[idx].scale.x = tmpx;
             buildingMeshes[idx].scale.y = tmpy;
             buildingMeshes[idx].scale.z = tmpz;
